effa5dd086904963943261819836f57134333a4a
john <me@example.com>
2019-12-07 13:37:03 -0500

    added functions

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,9 @@ fn index() -> NamedFile {
 }
 
 #[get("/room/<room>/<user_id>")]
-async fn room_stream(room: String, user_id: i32, rooms: State<'_, Rooms<Message>>) -> sse::SSE {
+async fn room_stream(room: String, user_id: i32, rooms: State<'_, Rooms<String, i32, Message>>) -> sse::SSE {
     // Subscribe to the room. 'subscription' is a Stream of Messages.
-    let mut subscription = rooms.subscribe(&room, user_id).await;
-
-    // let mut new_subscription = rooms.new_subscribe(&room, user_id);
+    let mut subscription = rooms.add_user(&room, user_id).await;
 
     // Create the SSE stream
     sse::with_writer(|mut writer| async move {
@@ -49,14 +47,14 @@ async fn room_stream(room: String, user_id: i32, rooms: State<'_, Rooms<Message>
 }
 
 #[post("/room/<room>", data="<form>")]
-async fn post_message(room: String, form: Form<Message>, rooms: State<'_, Rooms<Message>>) {
+async fn post_message(room: String, form: Form<Message>, rooms: State<'_, Rooms<String, i32, Message>>) {
     // Send the message to the requested room
-    rooms.send(&room, form.into_inner()).await;
+    rooms.broadcast(&room, form.into_inner()).await;
 }
 
 fn main() {
     rocket::ignite()
-        .manage(Rooms::<Message>::new())
+        .manage(Rooms::<String, i32, Message>::new())
         .mount("/", routes![index, room_stream, post_message])
         .launch()
         .expect("server quit unexpectedly")
--- src/rooms.rs
+++ src/rooms.rs
@@ -11,32 +11,54 @@ use tokio::stream::Stream;
 use tokio::sync::{mpsc};
 
 use dashmap::DashMap;
+use std::hash::Hash;
 
-pub struct Rooms<M> {
-    subscriptions: DashMap<String, DashMap<i32, mpsc::Sender<M>>>
+pub struct Rooms<R: Eq + Hash + Clone, U: Eq + Hash + Clone, M> {
+    subscriptions: DashMap<R, DashMap<U, mpsc::Sender<M>>>
 }
 
 pub struct Subscription<M>(mpsc::Receiver<M>);
 
-impl<M> Rooms<M> {
+impl<R: Eq + Hash + Clone, U: Eq + Hash + Clone, M> Rooms<R, U, M> {
     pub fn new() -> Self {
         Self {
             subscriptions: DashMap::new()
         }
     }
 
-    pub async fn subscribe(&self, room: &str, user_id: i32) -> Subscription<M> {
+    pub async fn add_user(&self, room: &R, user: U) -> Subscription<M> {
         let (tx, rx) = mpsc::channel(10);
         {
-            let room = self.subscriptions.entry(room.to_string()).or_insert(DashMap::new());
-            room.insert(user_id, tx);
+            let room = self.subscriptions.entry(room.clone()).or_insert(DashMap::new());
+            room.insert(user, tx);
         }
         Subscription(rx)
     }
+
+    pub fn remove_user(&self, room: &R, user: U) -> Option<(U, mpsc::Sender<M>)> {
+        if let Some(room) = self.subscriptions.get_mut(room) {
+            return room.remove(&user);
+        }
+
+        return None;
+
+        // Question: Does the above automatically drop the MPSC associated with the user?
+    }
+
+    pub fn contains_user(&self, room: &R, user: U) -> bool {
+        if let Some(room) = self.subscriptions.get_mut(room) {
+            return room.contains_key(&user);
+        }
+
+        // TODO: The above should remove the user if their connection is dropped
+        // Not sure how to do this other than sending a dummy message and seeing if it sent?
+
+        return false;
+    }
 }
 
-impl<M: Clone> Rooms<M> {
-    pub async fn send(&self, room: &str, message: M) { 
+impl<R: Eq + Hash + Clone, U: Eq + Hash + Clone, M: Clone> Rooms<R, U, M> {
+    pub async fn broadcast(&self, room: &R, message: M) { 
         if let Some(room) = self.subscriptions.get_mut(room) {
             let mut disconnects = vec![];
 
@@ -46,8 +68,8 @@ impl<M: Clone> Rooms<M> {
                 }
             }
 
-            for user_id in disconnects {
-                room.remove(&user_id);
+            for user in disconnects {
+                room.remove(&user);
             }
         }
     }
