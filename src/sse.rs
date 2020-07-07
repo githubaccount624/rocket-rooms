b2e182ec34c9b944582d8298906e3edb37d83d8b
githubaccount624 <d7603830@urhen.com>
2020-07-06 21:07:18 -0400

    Update sse.rs

--- src/sse.rs
+++ src/sse.rs
@@ -5,26 +5,31 @@ use std::task::{Context, Poll};
 
 use futures_util::stream::Stream;
 use rocket::request::Request;
-use rocket::response::{Responder, Response, ResultFuture};
+use rocket::response::{Responder, Response};
 use tokio::io::AsyncRead;
 
 #[derive(Clone, Debug)]
 pub struct Event {
-    pub serialized: Vec<u8>
-    // event, data, retry, and id fields
+    pub serialized: Vec<u8>,
+    pub id: u64
+    // event, data, retry, and id fields?
 }
 
 impl Event {
-    pub fn new(event: Option<String>, data: String) -> Self {
-        Self { serialized: Self::serialize(event, data) }
+    pub fn new(event: &str, data: String, id: u64) -> Self {
+        Self { serialized: Self::serialize(event, data, id), id }
     }
 
-    fn serialize(event: Option<String>, data: String) -> Vec<u8> {
-        let mut vec = Vec::with_capacity(6); // minimum size for an SSE message
+    fn serialize(event: &str, data: String, id: u64) -> Vec<u8> {
+        let mut vec = Vec::with_capacity(12); // minimum size for an SSE message
 
-        if let Some(event) = event {
+        vec.extend(b"id: ");
+        vec.extend(id.to_string().as_bytes());
+        vec.extend(b"\n");
+
+        if event.len() > 0 {
             vec.extend(b"event: ");
-            vec.extend(event.into_bytes());
+            vec.extend(event.as_bytes());
             vec.extend(b"\n");
         }
 
@@ -40,31 +45,29 @@ impl Event {
     }
 }
 
-pin_project_lite::pin_project! {
-    /// An SSE stream. This type implements `Responder`; see the
-    /// [`from_stream`] function for a usage example.
-    pub struct SSE<S> {
-        #[pin]
-        stream: S,
-        state: State,
+pub struct SSE<S>(S);
+
+pub fn from_stream<S: Stream<Item=Event>>(stream: S) -> SSE<S> {
+    SSE(stream)
+}
+
+#[rocket::async_trait]
+impl<'r, S: Stream<Item=Event> + Send + 'r> Responder<'r> for SSE<S> {
+    async fn respond_to(self, _req: &'r Request<'_>) -> rocket::response::Result<'r> {
+        Response::build()
+            .raw_header("Content-Type", "text/event-stream")
+            .raw_header("Cache-Control", "no-transform") // no-cache
+            .raw_header("Connection", "keep-alive")
+            .streamed_body(SSEReader { stream: self.0, state: State::Pending })
+            .ok()
     }
 }
 
-pub fn from_stream<S: Stream<Item=Event>>(stream: S) -> SSE<S> {
-    SSE { stream, state: State::Pending }
-}
-
-impl<'r, S: Stream<Item=Event> + Send + 'r> Responder<'r> for SSE<S> {
-    fn respond_to(self, _req: &'r Request<'_>) -> ResultFuture<'r> {
-        Box::pin(async move {
-            Response::build()
-                .raw_header("Content-Type", "text/event-stream")
-                .raw_header("Cache-Control", "no-transform") // no-cache
-                .raw_header("Expires", "0")
-                .raw_header("Connection", "keep-alive")
-                .streamed_body(self)
-                .ok()
-        })
+pin_project_lite::pin_project! {
+    struct SSEReader<S> {
+        #[pin]
+        stream: S,
+        state: State,
     }
 }
 
@@ -74,7 +77,7 @@ enum State {
     Done,
 }
 
-impl<S: Stream<Item=Event>> AsyncRead for SSE<S> {
+impl<S: Stream<Item=Event>> AsyncRead for SSEReader<S> {
     fn poll_read(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<Result<usize, io::Error>> {
         let mut this = self.project();
 
@@ -83,7 +86,7 @@ impl<S: Stream<Item=Event>> AsyncRead for SSE<S> {
         }
 
         loop {
-            match this.state {
+            match &mut this.state {
                 State::Pending => {
                     // Get the next buffer
                     match this.stream.as_mut().poll_next(cx) {
