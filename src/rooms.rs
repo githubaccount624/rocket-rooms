c575bf450367cd39d1fe66a7be4967ededa948be
githubaccount624 <d7603830@urhen.com>
2020-07-12 15:20:37 -0400

    Update rooms.rs

--- src/rooms.rs
+++ src/rooms.rs
@@ -8,6 +8,7 @@ use tokio::time;
 use std::time::Duration;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 
 use crate::sse::Event;
 
@@ -20,27 +21,57 @@ struct Mappings {
     user_to_subs: HashMap<i32, HashSet<String>>,
     room_to_subs: HashMap<String, HashSet<String>>,
     sub_to_client: HashMap<String, Client>,
-    sub_to_log: HashMap<String, Vec<u64>>,
-    id_to_event: HashMap<u64, Event>
+    event_log: VecDeque<Event>,
+    last_generated_id: u64
 }
 
-// try_send will either succeed immediately, and the client will get the event "eventually". Or it will fail, because events are being queued faster than they can be sent to the client, or because the client has disconnected
+const MAX_EVENT_LOG_SIZE: usize = 10000;
+const MAX_CLIENT_LOG_SIZE: usize = 500;
 
-pub struct Client {
-    consecutive_failures: u64,
-    sender: mpsc::Sender<Event>,
-    rooms: HashSet<String>
-}
+// try_send will either succeed immediately, and the client will get the event "eventually". Or it will fail,
+// because events are being queued faster than they can be sent to the client, or because the client has disconnected
 
 // Possible Ideas:
 // Replace String with Arc<str> and make cloning a simple atomic increment since don't mutate the strings afterwards?
 // Wrapping Room with a future aware mutex instead of using a channel?
 
-use std::sync::atomic::{AtomicU64, Ordering};
+pub struct Client {
+    consecutive_heartbeat_failures: u64,
+    sender: mpsc::Sender<Event>,
+    log: VecDeque<u64>,
+    rooms: HashSet<String>
+}
+
+pub trait VecDequeExt<T> {
+    fn push_drop(&mut self, val: T, cap: usize);
+}
+
+impl<T> VecDequeExt<T> for VecDeque<T> {
+    fn push_drop(&mut self, val: T, cap: usize) {
+        self.push_back(val);
+    
+        if self.len() > cap {
+            self.pop_front();
+        }
+    }
+}
+
+fn search_event_log(event_log: &VecDeque<Event>, id: u64) -> Option<Event> {
+	let slices = event_log.as_slices();
+	
+	if let Ok(idx) = slices.0.binary_search_by_key(&id, |k| k.id) {
+		return Some(slices.0[idx].clone());
+	}
+			
+	if let Ok(idx) = slices.1.binary_search_by_key(&id, |k| k.id) {
+		return Some(slices.1[idx].clone());
+	}
+	
+	return None;
+}
 
 pub struct Rooms {
-    tx: mpsc::Sender<Command>,
-    last_generated_id: AtomicU64
+    tx: mpsc::Sender<Command>
 }
 
 #[derive(Debug)]
@@ -51,8 +82,8 @@ enum Command {
     RoomJoinRoom { current_room: String, new_room: String },
     Leave { sub: String, room: String },
     Contains { sub: String, room: String, sender: oneshot::Sender<bool> },
-    SendRoom { room: String, event: Event },
-    SendUser { user_id: i32, event: Event },
+    SendRoom { room: String, event: &'static str, data: String },
+    SendUser { user_id: i32, event: &'static str, data: String },
     SendMissingEvents { sub: String, last_event_id: u64 },
     SendHeartbeat
 }
@@ -60,19 +91,13 @@ enum Command {
 pub struct Subscription(mpsc::Receiver<Event>);
 
 const TASK_SHUTDOWN_ERROR_MESSAGE: &'static str = "Permanent background task was shut down unexpectedly";
-const FAILURE_LIMIT: u64 = 100; // increase?
+const HEARTBEAT_FAILURE_LIMIT: u64 = 500;
 
 impl Rooms {
     pub fn new() -> Self {
         let (tx, rx) = mpsc::channel(1024);
         tokio::spawn(Self::background_task(rx));
-        Rooms { tx, last_generated_id: AtomicU64::new(1) }
-    }
-
-    pub fn create_event(&self, event: &str, data: String) -> Event {
-        let id = self.last_generated_id.fetch_add(1, Ordering::SeqCst);
-
-        Event::new(event, data, id)
+        Rooms { tx }
     }
 
     pub async fn subscribe(&self, sub: String, user_id: Option<i32>) -> Subscription {
@@ -107,12 +132,12 @@ impl Rooms {
         receiver.map(|member| { return member }).await.expect(TASK_SHUTDOWN_ERROR_MESSAGE)
     }
 
-    pub async fn send_room(&self, room: String, event: Event) {
-        self.tx.clone().send(Command::SendRoom { room, event }).await.expect(TASK_SHUTDOWN_ERROR_MESSAGE);
+    pub async fn send_room(&self, room: String, event: &'static str, data: String) {
+        self.tx.clone().send(Command::SendRoom { room, event, data }).await.expect(TASK_SHUTDOWN_ERROR_MESSAGE);
     }
 
-    pub async fn send_user(&self, user_id: i32, event: Event) {
-        self.tx.clone().send(Command::SendUser { user_id, event }).await.expect(TASK_SHUTDOWN_ERROR_MESSAGE);
+    pub async fn send_user(&self, user_id: i32, event: &'static str, data: String) {
+        self.tx.clone().send(Command::SendUser { user_id, event, data }).await.expect(TASK_SHUTDOWN_ERROR_MESSAGE);
     }
 
     pub async fn send_missing_events(&self, sub: String, last_event_id: u64) {
@@ -123,61 +148,64 @@ impl Rooms {
         tokio::spawn(Self::heartbeat_task(self.tx.clone(), heartbeat_interval_secs));
     }
     
-    fn helper_subscribe(mappings: &mut Mappings, sub: String, user_id: Option<i32>, tx: mpsc::Sender<Event>) {
+    fn helper_subscribe(m: &mut Mappings, sub: String, user_id: Option<i32>, tx: mpsc::Sender<Event>) {
         if let Some(user_id) = user_id {
-            let mut subs = HashSet::new();
+            let subs = m.user_to_subs.entry(user_id).or_insert(HashSet::new());
             subs.insert(sub.clone());
-
-            mappings.user_to_subs.insert(user_id, subs);
         }
 
-        mappings.sub_to_client.insert(sub, Client { sender: tx, consecutive_failures: 0, rooms: HashSet::new() });
+        if let Some(client) = m.sub_to_client.get_mut(&sub) {
+            client.sender = tx;
+            client.consecutive_heartbeat_failures = 0;
+        } else {
+            m.sub_to_client.insert(sub, Client { sender: tx, log: VecDeque::new(), consecutive_heartbeat_failures: 0, rooms: HashSet::new() });
+        }
     }
 
-    fn helper_join(mappings: &mut Mappings, room: String, sub: String) {
-        let subs = mappings.room_to_subs.entry(room.clone()).or_insert(HashSet::new());
+    fn helper_join(m: &mut Mappings, room: String, sub: String) {
+        let subs = m.room_to_subs.entry(room.clone()).or_insert(HashSet::new());
         subs.insert(sub.clone());
 
-        if let Some(client) = mappings.sub_to_client.get_mut(&sub) {
+        if let Some(client) = m.sub_to_client.get_mut(&sub) {
             client.rooms.insert(room);
         }
     }
 
-    fn helper_room_join_room(mappings: &mut Mappings, current_room: String, new_room: String) {
-        if let Some(subs) = mappings.room_to_subs.get(&current_room) {
+    fn helper_room_join_room(m: &mut Mappings, current_room: String, new_room: String) {
+        if let Some(subs) = m.room_to_subs.get(&current_room) {
             let cloned = subs.clone();
 
             for sub in cloned {
-                Self::helper_join(mappings, new_room.clone(), sub);
+                Self::helper_join(m, new_room.clone(), sub);
             }
         }
     }
 
-    fn helper_join_user(mappings: &mut Mappings, room: String, user_id: i32) {
-        if let Some(subs) = mappings.user_to_subs.get_mut(&user_id) {
+    fn helper_join_user(m: &mut Mappings, room: String, user_id: i32) {
+        if let Some(subs) = m.user_to_subs.get_mut(&user_id) {
             for sub in subs.iter() {
-                if let Some(client) = mappings.sub_to_client.get_mut(sub) {
+                if let Some(client) = m.sub_to_client.get_mut(sub) {
                     client.rooms.insert(room.clone());
                 }
 
-                let room_subs = mappings.room_to_subs.entry(room.clone()).or_insert(HashSet::new());
+                let room_subs = m.room_to_subs.entry(room.clone()).or_insert(HashSet::new());
                 room_subs.insert(sub.clone());
             }
         }
     }
 
-    fn helper_leave(mappings: &mut Mappings, room: String, sub: String) {
-        if let Some(subs) = mappings.room_to_subs.get_mut(&room) {
+    fn helper_leave(m: &mut Mappings, room: String, sub: String) {
+        if let Some(subs) = m.room_to_subs.get_mut(&room) {
             subs.remove(&sub);
         }
 
-        if let Some(client) = mappings.sub_to_client.get_mut(&sub) {
+        if let Some(client) = m.sub_to_client.get_mut(&sub) {
             client.rooms.remove(&room);
         }
     }
 
-    async fn helper_contains(mappings: &mut Mappings, room: String, sub: String, sender: oneshot::Sender<bool>) { 
-        if let Some(subs) = mappings.room_to_subs.get(&room) {
+    async fn helper_contains(m: &mut Mappings, room: String, sub: String, sender: oneshot::Sender<bool>) { 
+        if let Some(subs) = m.room_to_subs.get(&room) {
             sender.send(subs.contains(&sub)).expect(TASK_SHUTDOWN_ERROR_MESSAGE);
             
             return;
@@ -186,34 +214,36 @@ impl Rooms {
         sender.send(false).expect(TASK_SHUTDOWN_ERROR_MESSAGE);
     }
 
-    fn send_client_message(sub_to_log: &mut HashMap<String, Vec<u64>>, id_to_event: &mut HashMap<u64, Event>, sub: String, client: &mut Client, event: Event) -> bool {
-        let log = sub_to_log.entry(sub).or_insert(Vec::new());
-        log.push(event.id);
+    fn send_client_message(event_log: &mut VecDeque<Event>, sub: String, client: &mut Client, event: Event) -> bool {
+        client.log.push_drop(event.id, MAX_CLIENT_LOG_SIZE);
 
-        if id_to_event.get(&event.id).is_none() {
-            id_to_event.insert(event.id, event.clone());
+        let sended_successfully = client.sender.try_send(event).is_ok();
+
+        if sended_successfully {
+            client.consecutive_heartbeat_failures = 0;
         }
 
-        let failed_to_send = client.sender.try_send(event).is_err();
-
-        if failed_to_send {
-            client.consecutive_failures += 1;
-        } else {
-            client.consecutive_failures = 0;
-        }
-
-        client.consecutive_failures < FAILURE_LIMIT
+        client.consecutive_heartbeat_failures < HEARTBEAT_FAILURE_LIMIT
     }
 
-    async fn helper_send_room(mappings: &mut Mappings, room: String, event: Event) { 
+    fn create_new_event(m: &mut Mappings, event: &str, data: String) -> Event {
+        let event = Event::new(event, data, m.last_generated_id);
+        m.last_generated_id += 1;
+
+        m.event_log.push_drop(event.clone(), MAX_EVENT_LOG_SIZE);
+
+        event
+    }
+
+    async fn helper_send_room(m: &mut Mappings, room: String, event: &str, data: String) { 
         let mut disconnects = vec![];
+
+        let event = Self::create_new_event(m, event, data);
         
-        if let Some(subs) = mappings.room_to_subs.get(&room) {
+        if let Some(subs) = m.room_to_subs.get(&room) {
             for sub in subs.iter() {
-                if let Some(mut client) = mappings.sub_to_client.get_mut(sub) {
-                    println!("sending a message to this client {} to this room {}", sub, room);
-
-                    if !Self::send_client_message(&mut mappings.sub_to_log, &mut mappings.id_to_event, sub.clone(), &mut client, event.clone()) {
+                if let Some(mut client) = m.sub_to_client.get_mut(sub) {
+                    if !Self::send_client_message(&mut m.event_log, sub.clone(), &mut client, event.clone()) {
                         disconnects.push(sub.to_string());
                     }
                 }
@@ -221,17 +251,19 @@ impl Rooms {
         }
 
         for sub in disconnects {
-            Self::clean_up_user(mappings, sub);
+            Self::clean_up_user(m, sub);
         }
     }
 
-    async fn helper_send_user(mappings: &mut Mappings, user_id: i32, event: Event) { 
+    async fn helper_send_user(m: &mut Mappings, user_id: i32, event: &str, data: String) { 
         let mut disconnects = vec![];
 
-        if let Some(subs) = mappings.user_to_subs.get_mut(&user_id) {
+        let event = Self::create_new_event(m, event, data);
+
+        if let Some(subs) = m.user_to_subs.get_mut(&user_id) {
             for sub in subs.iter() {
-                if let Some(mut client) = mappings.sub_to_client.get_mut(sub) {
-                    if !Self::send_client_message(&mut mappings.sub_to_log, &mut mappings.id_to_event, sub.clone(), &mut client, event.clone()) {
+                if let Some(mut client) = m.sub_to_client.get_mut(sub) {
+                    if !Self::send_client_message(&mut m.event_log, sub.clone(), &mut client, event.clone()) {
                         disconnects.push(sub.to_string());
                     }
                 }
@@ -239,62 +271,78 @@ impl Rooms {
         }
 
         for sub in disconnects {
-            Self::clean_up_user(mappings, sub);
+            Self::clean_up_user(m, sub);
         }
     }
 
-    async fn helper_send_missing_events(mappings: &mut Mappings, sub: String, last_event_id: u64) {
-        if let Some(client) = mappings.sub_to_client.get_mut(&sub) {
-            if let Some(log) = mappings.sub_to_log.get(&sub) {
-                for id in log {
-                    if *id > last_event_id { // change to binary search?
-                        if let Some(event) = mappings.id_to_event.get(id) {
-                            if client.sender.try_send(event.clone()).is_err() {
-                                // delete?
-                            }
+    fn send_refresh_event(client: &mut Client) {
+        let refresh_event = Event::new("sse-refresh", "refresh".to_string(), 0);
+
+        if client.sender.try_send(refresh_event).is_err() {
+            // ?
+        }
+    }
+
+    async fn helper_send_missing_events(m: &mut Mappings, sub: String, last_event_id: u64) {
+        if let Some(client) = m.sub_to_client.get_mut(&sub) {
+            for id in &client.log {
+                if client.log.len() >= MAX_CLIENT_LOG_SIZE && last_event_id < client.log[0] { // there's a gap in event log. refresh
+                    Self::send_refresh_event(client);
+
+                    return;
+                } 
+
+                if *id > last_event_id { // change to binary search?
+                    if let Some(event) = search_event_log(&m.event_log, *id) {
+                        if client.sender.try_send(event).is_err() {
+                            // delete?
                         }
+                    } else { // event not found in global log. refresh
+                        Self::send_refresh_event(client);
+
+                        return;
                     }
                 }
             }
         }
     }
 
-    async fn helper_send_heartbeat(mappings: &mut Mappings) {
+    async fn helper_send_heartbeat(m: &mut Mappings) {
         let mut disconnects = vec![];
 
-        let heartbeat_message = Event::new("hb", "\n\n".to_string(), 0); // change from hb to empty message
+        let heartbeat_message = Event::new("hb", "\n\n".to_string(), 0); // change from hb to empty message?
 
-        for (sub, client) in mappings.sub_to_client.iter_mut() {
+        for (sub, client) in m.sub_to_client.iter_mut() {
             if client.sender.try_send(heartbeat_message.clone()).is_err() {
-                client.consecutive_failures += 1;
+                client.consecutive_heartbeat_failures += 1;
 
-                if client.consecutive_failures > FAILURE_LIMIT { // refactor this to a shared function?
+                if client.consecutive_heartbeat_failures > HEARTBEAT_FAILURE_LIMIT {
                     disconnects.push(sub.to_string());
                 }
             } else {
-                client.consecutive_failures = 0;
+                client.consecutive_heartbeat_failures = 0;
             }
         }
 
         for sub in disconnects {
-            Self::clean_up_user(mappings, sub);
+            Self::clean_up_user(m, sub);
         }
     }
 
-    fn clean_up_user(mappings: &mut Mappings, sub: String) {
+    fn clean_up_user(m: &mut Mappings, sub: String) {
         let splitted: Vec<_> = sub.split("-").collect();
 
         if splitted.len() > 0 {
             let user_id = splitted[0].parse::<i32>().unwrap();
 
-            if let Some(subs) = mappings.user_to_subs.get_mut(&user_id) {
+            if let Some(subs) = m.user_to_subs.get_mut(&user_id) {
                 subs.remove(&sub);
             }
         }
 
-        if let Some(client) = mappings.sub_to_client.remove(&sub) {
+        if let Some(client) = m.sub_to_client.remove(&sub) {
             for room in client.rooms.iter() {
-                if let Some(subs) = mappings.room_to_subs.get_mut(room) {
+                if let Some(subs) = m.room_to_subs.get_mut(room) {
                     subs.remove(&sub);
                 }
             }
@@ -312,45 +360,45 @@ impl Rooms {
     }
 
     async fn background_task(mut rx: mpsc::Receiver<Command>) {
-        let mut mappings = Mappings {
+        let mut m = Mappings {
             user_to_subs: HashMap::new(),
             room_to_subs: HashMap::new(),
             sub_to_client: HashMap::new(),
-            sub_to_log: HashMap::new(),
-            id_to_event: HashMap::new()
+            event_log: VecDeque::new(),
+            last_generated_id: 1
         };
 
         while let Some(command) = rx.recv().await {
             match command {
                 Command::Subscribe { sub, user_id, tx } => {
-                    Self::helper_subscribe(&mut mappings, sub, user_id, tx);
+                    Self::helper_subscribe(&mut m, sub, user_id, tx);
                 }
                 Command::Join { room, sub } => {
-                    Self::helper_join(&mut mappings, room, sub);
+                    Self::helper_join(&mut m, room, sub);
                 }
                 Command::JoinUser { room, user_id } => {
-                    Self::helper_join_user(&mut mappings, room, user_id);
+                    Self::helper_join_user(&mut m, room, user_id);
                 }
                 Command::RoomJoinRoom { current_room, new_room } => {
-                    Self::helper_room_join_room(&mut mappings, current_room, new_room);
+                    Self::helper_room_join_room(&mut m, current_room, new_room);
                 }
                 Command::Leave { room, sub } => {
-                    Self::helper_leave(&mut mappings, room, sub);
+                    Self::helper_leave(&mut m, room, sub);
                 }
                 Command::Contains { room, sub, sender } => {
-                    Self::helper_contains(&mut mappings, room, sub, sender).await;
+                    Self::helper_contains(&mut m, room, sub, sender).await;
                 }
-                Command::SendRoom { room, event } => {
-                    Self::helper_send_room(&mut mappings, room, event).await;
+                Command::SendRoom { room, event, data } => {
+                    Self::helper_send_room(&mut m, room, event, data).await;
                 }
-                Command::SendUser { user_id, event } => {
-                    Self::helper_send_user(&mut mappings, user_id, event).await;
+                Command::SendUser { user_id, event, data } => {
+                    Self::helper_send_user(&mut m, user_id, event, data).await;
                 }
                 Command::SendMissingEvents { sub, last_event_id } => {
-                    Self::helper_send_missing_events(&mut mappings, sub, last_event_id).await;
+                    Self::helper_send_missing_events(&mut m, sub, last_event_id).await;
                 }
                 Command::SendHeartbeat => {
-                    Self::helper_send_heartbeat(&mut mappings).await;
+                    Self::helper_send_heartbeat(&mut m).await;
                 }
             }
         }
